6214fe3075d1c14256c4a2668139b04a1f1ed87e
Elddar00 <eldaridrizovic00@gmail.com>
2025-02-16 20:56:44 +0100

    Assembly Endgame V2

---
+++ src/App.js
@@ -0,0 +1,183 @@
+import { useState } from "react";
+import "./App.css";
+import { languages } from "./languages";
+import clsx from "clsx";
+import { getFarewellText, getRandomWord } from "./utils";
+import Confetti from "react-confetti";
+
+/**
+ * Backlog:
+ *
+ * - Confetti drop when the user wins
+ */
+
+export default function AssemblyEndgame() {
+  const [currentWord, setCurrentWord] = useState(() => getRandomWord());
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  // console.log(guessedLetters);\
+
+  const numGuessesLeft = languages.length - 1;
+  const wrongGuessCount = guessedLetters.filter(
+    (letter) => !currentWord.includes(letter)
+  ).length;
+
+  const isGameWon = currentWord
+    .split("")
+    .every((letter) => guessedLetters.includes(letter));
+  const isGameLost = wrongGuessCount >= numGuessesLeft;
+  const isGameOver = isGameWon || isGameLost;
+
+  const lastGuessedLetter = guessedLetters[guessedLetters.length - 1];
+
+  const isLastGuessIncorrect =
+    lastGuessedLetter && !currentWord.includes(lastGuessedLetter);
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  // first way
+  // function addGuessedLetter(letter) {
+  //   setGuessedLetters((prevLetters) => {
+  //     const letterSet = new Set(prevLetters);
+  //     letterSet.add(letter);
+  //     return Array.from(letterSet);
+  //   });
+  // }
+
+  function addGuessedLetter(letter) {
+    setGuessedLetters((prevLetters) =>
+      prevLetters.includes(letter) ? prevLetters : [...prevLetters, letter]
+    );
+  }
+
+  function startNewGame() {
+    setCurrentWord(getRandomWord());
+    setGuessedLetters([]);
+  }
+
+  const langugeElements = languages.map((lang, index) => {
+    const isLanguageLost = index < wrongGuessCount;
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color,
+    };
+    const className = clsx("chip", isLanguageLost && "lost");
+    return (
+      <span className={className} style={styles} key={lang.name}>
+        {lang.name}
+      </span>
+    );
+  });
+
+  const letterElements = currentWord.split("").map((letter, index) => {
+    const shouldRevealLetter = isGameLost || guessedLetters.includes(letter);
+    const letterClassName = clsx(
+      isGameLost && !guessedLetters.includes(letter) && "missed-letter"
+    );
+    return (
+      <span key={index} className={letterClassName}>
+        {shouldRevealLetter ? letter.toUpperCase() : ""}
+      </span>
+    );
+  });
+
+  const keyboardElements = alphabet.split("").map((letter) => {
+    const isGuessed = guessedLetters.includes(letter);
+    const isCorrect = isGuessed && currentWord.includes(letter);
+    const isWrong = isGuessed && !currentWord.includes(letter);
+
+    const className = clsx({
+      correct: isCorrect,
+      wrong: isWrong,
+    });
+
+    // console.log(className);
+
+    return (
+      <button
+        className={className}
+        key={letter}
+        disabled={isGameOver}
+        onClick={() => addGuessedLetter(letter)}
+      >
+        {letter.toUpperCase()}
+      </button>
+    );
+  });
+
+  const gameStatusClass = clsx("game-status", {
+    won: isGameWon,
+    lost: isGameLost,
+    farewell: !isGameOver && isLastGuessIncorrect,
+  });
+
+  function renderGameStatus() {
+    if (!isGameOver && isLastGuessIncorrect) {
+      return (
+        <p className="farewell-message">
+          {getFarewellText(languages[wrongGuessCount - 1].name)}
+        </p>
+      );
+    }
+
+    if (isGameWon) {
+      return (
+        <>
+          <h2>You win!</h2>
+          <p>Well done! 🎊</p>
+        </>
+      );
+    }
+    if (isGameLost) {
+      return (
+        <>
+          <h2>Game over!</h2>
+          <p>You lose ! Better start learning Assembly !</p>
+        </>
+      );
+    }
+
+    return null;
+  }
+
+  return (
+    <main>
+      {isGameWon && <Confetti recycle={false} numberOfPieces={1000} />}
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the world within 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </header>
+      {/*Combined  visually-hidden aria-live region for status updates*/}
+      <section aria-live="polite" role="status" className={gameStatusClass}>
+        {renderGameStatus()}
+      </section>
+      <section className="language-chips">{langugeElements}</section>
+      <section className="word">{letterElements}</section>
+      <section className="sr-only" aria-live="polite" role="status">
+        <p>
+          {currentWord.includes(lastGuessedLetter)
+            ? `Correct! The letter ${lastGuessedLetter} is in the word`
+            : `Sorry! The letter ${lastGuessedLetter} is not in the word`}
+          You have {numGuessesLeft} attempts left.
+        </p>
+        <p>
+          Current word:{" "}
+          {currentWord
+            .split("")
+            .map((letter) =>
+              guessedLetters.includes(letter) ? letter + "." : "blank."
+            )
+            .join(" ")}{" "}
+        </p>
+      </section>
+      <section className="keyboard">{keyboardElements}</section>
+      {isGameOver && (
+        <button className="new-game" onClick={startNewGame}>
+          New Game
+        </button>
+      )}
+    </main>
+  );
+}
